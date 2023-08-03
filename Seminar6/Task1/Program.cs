c75a08757a14d7639431d8fe70d09a85e29d351f
Nikita <nikitander1@gmail.com>
2023-08-03 14:15:27 +0300

    Выполнил задание 6го семинара

---
+++ Seminar6/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int CountPositiveNumbers(int size)
+{
+    int count=0;
+    for (int i = 0; i < size; i++)
+    {
+        int a = int.Parse(Console.ReadLine());
+        if (a > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Сколько чисел ты хочешь ввести?");
+int size = int.Parse(Console.ReadLine());
+int numberOfPositiveValues = CountPositiveNumbers(size);
+if (numberOfPositiveValues != 0) Console.WriteLine($"Количество чисел больше нуля равно {numberOfPositiveValues}");
+else Console.WriteLine("Пользователь не ввел чисел больше нуля");
---
+++ Seminar6/Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Введите k1, b1, k2, b2");
+int k1 = int.Parse(Console.ReadLine());
+int b1 = int.Parse(Console.ReadLine());
+int k2 = int.Parse(Console.ReadLine());
+int b2 = int.Parse(Console.ReadLine());
+double x = (double) (b2-b1) / (k1-k2);
+double y = (double) (k1*x+b1);
+Console.WriteLine($"Координата точки пересечения равна ({x};{y})" );
